1f278326afeee8dd050c4e382a482da9a96761ba
varshabaddi12 <varshabaddi12@gmail.com>
2026-04-11 00:06:23 +0530

    Update settings.py

--- parking/parksmart1/settings.py
+++ parking/parksmart1/settings.py
@@ -5,7 +5,7 @@ Django settings for parksmart project.
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
